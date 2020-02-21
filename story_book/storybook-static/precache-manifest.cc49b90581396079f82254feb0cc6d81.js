6d3a3cbdef0cb64a9ac0368d2fd36808a963eb48
Andrew Frazier <acorbettfrazier@gmail.com>
2020-02-21 16:12:24 -0500

    added storybook build script

---
+++ story_book/storybook-static/precache-manifest.cc49b90581396079f82254feb0cc6d81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00c944d13e176255a669347bd79084a9",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.6a70ce579a04308b4d5b.bundle.js"
+  },
+  {
+    "url": "runtime~main.6a70ce579a04308b4d5b.bundle.js"
+  },
+  {
+    "revision": "b0906132d645acc2e4ff",
+    "url": "static/css/main.beef7770.chunk.css"
+  },
+  {
+    "url": "vendors~main.6a70ce579a04308b4d5b.bundle.js"
+  },
+  {
+    "url": "vendors~main.6a70ce579a04308b4d5b.bundle.js.LICENSE.txt"
+  }
+]);
