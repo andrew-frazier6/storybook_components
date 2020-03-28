16abed4148ace9e844acab707afb59ccdb8513e8
Andrew Frazier <acorbettfrazier@gmail.com>
2020-03-28 16:16:28 -0400

    finished

--- src/Components /Voucher/Voucher.stories.js
+++ src/Components /Voucher/Voucher.stories.js
@@ -4,7 +4,7 @@ import Voucher from "./Voucher";
 import "./Voucher.css";
 
 storiesOf("Voucher", module)
-  .add("Voucher", () => <Voucher small label="Redeem" type="voucher" white />)
+  .add("Voucher", () => <Voucher label="Redeem" type="voucher" white />)
   .add("Voucher-medium", () => (
     <Voucher label="Redeem" type="voucher" medium white />
   ));
